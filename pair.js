2e5e6cd762fcf0df688bf81ef16d154300c13ed2
kingplayboi <isaacmaingi067@gmail.com>
2026-07-02 02:10:53 +0300

    Fix broken duplicate session code and normalize naming

--- pair.js
+++ pair.js
@@ -5,7 +5,7 @@ const express = require('express');
 const fs = require('fs');
 const pino = require('pino');
 const {
-    default: Isak_kingpin,
+    default: Isak_Kingpin,
     useMultiFileAuthState,
     fetchLatestBaileysVersion,
     delay,
@@ -31,45 +31,46 @@ router.get('/', async (req, res) => {
         console.log(version);
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
         try {
-            const Pair_Code_By_Isak_kingpin = Isak_kingpin({
-          auth: {
+            const Pair_Code_By_Isak_Kingpin = Isak_Kingpin({
+                auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
                 },
-        version,
-        printQRInTerminal: false,
-        logger: pino({
-          level: 'silent',
-        }),
-        browser: Browsers.windows('Edge'),
-      })
+                version,
+                printQRInTerminal: false,
+                logger: pino({
+                    level: 'silent',
+                }),
+                browser: Browsers.windows('Edge'),
+            })
             if (!Pair_Code_By_Isak_Kingpin.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
                 const custom = "BLIZZARD";
-                const code = await Pair_Code_By_Isak_kingpin.requestPairingCode(num,custom);
+                const code = await Pair_Code_By_Isak_Kingpin.requestPairingCode(num, custom);
 
                 if (!res.headersSent) {
                     await res.send({ code });
                 }
             }
 
-            Pair_Code_By_Isak_kingpin.ev.on('creds.update', saveCreds);
-            Pair_Code_By_Isak_kingpin.ev.on('connection.update', async (s) => {
+            Pair_Code_By_Isak_Kingpin.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Isak_Kingpin.ev.on('connection.update', async (s) => {
                 const { connection, lastDisconnect } = s;
                 if (connection === 'open') {
-const session = await Pair_Code_By_Isak_kingpin.sendMessage(Pair_Code_By_Isak_kingpin.user.id, { text: 'ISAAC-MD:~' + b64data });                    await delay(50000);
+                    await Pair_Code_By_Isak_Kingpin.sendMessage(Pair_Code_By_Isak_Kingpin.user.id, { text: '𝐀 𝐦𝐨𝐦𝐞𝐧𝐭 𝐰𝐚𝐢𝐭 𝐟𝐨𝐫 𝐲𝐨𝐮𝐫 𝐬𝐞𝐬𝐬𝐢𝐨𝐧...' });
+                    await delay(50000);
                     const data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(8000);
                     const b64data = Buffer.from(data).toString('base64');
-                    const session = await Pair_Code_By_Isak_kingpin.sendMessage(Pair_Code_By_Isak_kingpin.user.id, { text: 'ISAAC-MD:~' + b64data });
+                    const session = await Pair_Code_By_Isak_Kingpin.sendMessage(Pair_Code_By_Isak_Kingpin.user.id, { text: 'ISAAC-MD:~' + b64data });
 
                     // Send random message after session
                     const Textt = "```ISAAC-MD has been linked to your WhatsApp account! Above is your session.\n\nCopy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nISAAC~~ +254754574642\n\nAnd don't forget to fork and star our repo🎃,\n\nhttps://github.com/kingplayboi/ISAAC/fork.\n\nGoodluck 🎉. ```"
-                    await Pair_Code_By_Isak_kingpin.sendMessage(Pair_Code_By_Isak_kingpin.user.id, { text: Textt }, { quoted: session });
+                    await Pair_Code_By_Isak_Kingpin.sendMessage(Pair_Code_By_Isak_Kingpin.user.id, { text: Textt }, { quoted: session });
 
                     await delay(100);
-                    await Pair_Code_By_Isak_kingpin.ws.close();
+                    await Pair_Code_By_Isak_Kingpin.ws.close();
                     removeFile('./temp/' + id);
                 } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
                     await delay(10000);
@@ -89,4 +90,3 @@ const session = await Pair_Code_By_Isak_kingpin.sendMessage(Pair_Code_By_Isak_ki
 });
 
 module.exports = router;
-
